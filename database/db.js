3763155d39e4308d8ea305472afa2c35e53f69ae
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-26 22:57:56 +0900

    beanstalk db config test

--- database/db.js
+++ database/db.js
@@ -1,32 +1,12 @@
-// let connection;
-
-// async function handleConnectDB() {
-//   connection = await mysql.createConnection(db_config);
-
-//   connection.connect((err) => {
-//     if (err) {
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleConnectDB, 2000);
-//     }
-//     console.log('Mysql Database Connected...');
-//   });
-//   connection.on('error', (err) => {
-//     console.log('db error', err);
-//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//       handleConnectDB();
-//     }
-//     else {
-//       throw err;
-//     }
-//   });
-// }
-
-// handleConnectDB();
-
-// module.exports = connection;
-
 const mysql = require('mysql2');
 require('dotenv').config();
+
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
 
 const db_config = {
   host: process.env.DB_HOST,
