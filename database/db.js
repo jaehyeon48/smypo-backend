563f60638acfd5ac4aefea89420d1b82dd7cd3e7
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-26 23:09:12 +0900

    beanstalk db config test completed - deleted console.log

--- database/db.js
+++ database/db.js
@@ -1,13 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-})
-
 const db_config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,7 +8,6 @@ const db_config = {
   database: process.env.DB_NAME,
   waitForConnections: true
 };
-
 
 const pool = mysql.createPool(db_config);
 
