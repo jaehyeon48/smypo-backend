2b94a9d66f6776a101efbe48737c7500abdc7702
jaehyeon48 <imjaehyeon48@gmail.com>
2021-03-17 19:59:51 +0900

    refresh token logic production mode

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -28,7 +28,7 @@ async function authMiddleware(req, res, next) {
         };
         const newAccessToken = jwt.sign(jwtPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
         /* !!!!!!!!!!!!!!! sameSite should be 'strict' in production mode. !!!!!!!!!!!!!!! */
-        res.cookie('UAAT', newAccessToken, { httpOnly: true, sameSite: 'none', secure: true });
+        res.cookie('UAAT', newAccessToken, { httpOnly: true, sameSite: 'strict', secure: true });
         req.user = decodedRefresh.user;
         next();
       }
