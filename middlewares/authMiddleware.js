417978719d9cfbd8a324a38afe6bf2b903873c82
jaehyeon48 <imjaehyeon48@gmail.com>
2021-05-25 23:12:10 +0900

    handle expired session

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 
 // not use refresh token method for now
 async function authMiddleware(req, res, next) {
-  // UART for User Authentication Refresh Token
   // UAAT for User Authentication Access Token
-  // const refreshToken = req.cookies.UART;
   const accessToken = req.cookies.UAAT;
   if (!accessToken) {
     return res.status(401).json({ errorMsg: 'No token, authorization denied' });
@@ -24,24 +22,9 @@ async function authMiddleware(req, res, next) {
     next();
   } catch (error) {
     // if the access token has expired
-    // if (refreshToken !== null && error.name === 'TokenExpiredError') {
-    //   const decodedRefresh = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-    //   const [refreshFromDB] = await pool.query('SELECT token FROM refreshToken WHERE userId = ?', [decodedRefresh.user.id]);
-    //   // if the current refresh token from the user is the same as refresh token in the DB,
-    //   // issue a new access token
-    //   if (refreshToken === refreshFromDB[0].token) {
-    //     const jwtPayload = {
-    //       user: { id: decodedRefresh.user.id }
-    //     };
-    //     const newAccessToken = jwt.sign(jwtPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
-    //     /* !!!!!!!!!!!!!!! sameSite should be 'strict' in production mode. !!!!!!!!!!!!!!! */
-    //     res.cookie('UAAT', newAccessToken, { httpOnly: true, sameSite: process.env.SAME_SITE, secure: true });
-    //     req.user = decodedRefresh.user;
-    //     next();
-    //   }
-    // } else {
-    //   return res.status(401).json({ errorMsg: 'Token is invalid' });
-    // }
+    if (error.name === 'TokenExpiredError') {
+      return res.status(200).json(-999);
+    }
     return res.status(401).json({ errorMsg: 'Token is invalid' });
   }
 }
